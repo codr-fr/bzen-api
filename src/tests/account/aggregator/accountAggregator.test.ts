5d6e2b7ed86e73ec9d8da7c753afa77cac739863
Guillaume Aveline <guillaume@codr.fr>
2023-02-03 10:45:50 +0100

    add accountaggregator test and skip repository tests

---
+++ src/tests/account/aggregator/accountAggregator.test.ts
@@ -0,0 +1,88 @@
+import { v4 } from "uuid"
+import { AccountAggregator } from "../../../domain/account/aggregator/accountAggregator"
+import { AccountCreatedEvent } from "../../../domain/account/event/accountCreatedEvent"
+import { AccountCreditedEvent } from "../../../domain/account/event/accountCreditedEvent"
+import { AccountDebitedEvent } from "../../../domain/account/event/accountDebitedEvent"
+import { UserAggregator } from "../../../domain/user/aggregator/userAggregator"
+import { UserRegistredEvent } from "../../../domain/user/event/userRegistredEvent"
+import { UserUpdatedEvent } from "../../../domain/user/event/userUpdatedEvent"
+
+/*
+case ACCOUNT_CREATED_EVENT:
+    this.applyCreateAccountEvent(<AccountCreatedEvent>event)
+    break
+
+case ACCOUNT_CREDITED_EVENT:
+    this.applyCreditAccountEvent(<AccountCreditedEvent>event)
+    break
+
+case ACCOUNT_DEBITED_EVENT:
+    this.applyDebitAccountEvent(<AccountDebitedEvent>event)
+    break
+
+case ACCOUNT_ATTACHED_EVENT:
+    this.applyAttachAccountEvent(<AccountAttachedEvent>event)
+    break
+*/
+
+describe('AccountAggregator', () => {
+    let accountId: string
+    let aggreagator: AccountAggregator
+
+    beforeEach(() => {
+        accountId = v4()
+        aggreagator = new AccountAggregator(accountId)
+    })
+
+    test("Test default values", () => {
+        expect(aggreagator.id).toStrictEqual(accountId)
+        expect(aggreagator.currentBalance).toStrictEqual(0)
+        expect(aggreagator.estimatedBalance).toStrictEqual(0)
+        expect(Object.keys(aggreagator.permissions).length).toStrictEqual(0)
+    })
+
+    test("Account creation set current balance", () => {
+        aggreagator.applyEvents([
+            new AccountCreatedEvent(accountId, 42),
+        ])
+
+        expect(aggreagator.currentBalance).toStrictEqual(42)
+        expect(aggreagator.estimatedBalance).toStrictEqual(0)
+    })
+
+    test("Account credit", () => {
+        aggreagator.applyEvents([
+            new AccountCreatedEvent(accountId, 100),
+            new AccountCreditedEvent(accountId, 40),
+        ])
+
+        expect(aggreagator.currentBalance).toStrictEqual(140)
+        expect(aggreagator.estimatedBalance).toStrictEqual(0)
+    })
+
+    test("Account debit", () => {
+        aggreagator.applyEvents([
+            new AccountCreatedEvent(accountId, 100),
+            new AccountDebitedEvent(accountId, 40),
+        ])
+
+        expect(aggreagator.currentBalance).toStrictEqual(60)
+        expect(aggreagator.estimatedBalance).toStrictEqual(0)
+    })
+
+    test("Account multi operations", () => {
+        aggreagator.applyEvents([
+            new AccountCreatedEvent(accountId, 1000),
+            new AccountCreditedEvent(accountId, 40),
+            new AccountCreditedEvent(accountId, 40),
+            new AccountDebitedEvent(accountId, 20),
+            new AccountCreditedEvent(accountId, 100),
+            new AccountDebitedEvent(accountId, 50),
+            new AccountDebitedEvent(accountId, 70),
+        ])
+
+        expect(aggreagator.currentBalance).toStrictEqual(1040)
+        expect(aggreagator.estimatedBalance).toStrictEqual(0)
+    })
+})
+
---
+++ src/tests/user/repository/index.test.ts
@@ -0,0 +1,38 @@
+// Must be like this or it fails on runtime
+const mockingoose = require("mockingoose");
+
+import { findAllUsers } from '../../../domain/user/repository';
+import { Event } from '../../../model/event';
+
+// TBD if usefull
+describe.skip('User Repository', () => {
+    
+    mockingoose(Event).toReturn([
+        {
+          title: 'Book 1',
+          author: {
+            firstname: 'John',
+            lastname: 'Doe'
+          },
+          year: 2021,
+        },
+        {
+          title: 'Book 2',
+          author: {
+            firstname: 'Jane',
+            lastname: 'Doe'
+          },
+          year: 2022,
+        }
+      ], 'find');
+      
+
+    test("Should return all users registred", () => {
+
+        const users = findAllUsers()
+
+        console.log(users)
+
+        expect(1).toStrictEqual(2)
+    })
+})
