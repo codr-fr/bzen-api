19db9d68a4402599952e2d48247c971785935be6
Guillaume Aveline <guillaume@codr.fr>
2023-02-04 14:42:56 +0100

    repositories do not need to filter events

--- src/domain/account/repository/index.ts
+++ src/domain/account/repository/index.ts
@@ -2,9 +2,6 @@ import db from '../../../database/mongoose'
 import { Event } from '../../../model/event'
 import { AccountAggregator } from '../aggregator/accountAggregator'
 import { AccountsAggregator } from '../aggregator/accountsAggregator'
-import { ACCOUNT_ATTACHED_EVENT } from '../event/accountAttachedEvent'
-import { ACCOUNT_CREATED_EVENT } from '../event/accountCreatedEvent'
-import { ACCOUNT_DETACHED_EVENT } from '../event/accountDettachedEvent'
 
 export const findAccount = async (accountId: string): Promise<AccountAggregator> => {
   await db()
@@ -14,9 +11,6 @@ export const findAccount = async (accountId: string): Promise<AccountAggregator>
 
 export const findAccountByUser = async (userId: string): Promise<AccountAggregator[]> => {
   await db()
-  const events = await Event.find({
-    name: { $in: [ACCOUNT_CREATED_EVENT, ACCOUNT_ATTACHED_EVENT, ACCOUNT_DETACHED_EVENT] }
-  }).exec()
-
+  const events = await Event.find().exec()
   return new AccountsAggregator().applyEvents(events).aggregators.filter((account) => userId in account.permissions)
 }
--- src/domain/user/repository/index.ts
+++ src/domain/user/repository/index.ts
@@ -2,14 +2,10 @@ import db from '../../../database/mongoose'
 import { Event } from '../../../model/event'
 import { UserAggregator } from '../aggregator/userAggregator'
 import { UsersAggregator } from '../aggregator/usersAggregator'
-import { USER_REGISTRED_EVENT } from '../event/userRegistredEvent'
-import { USER_UPDATED_EVENT } from '../event/userUpdatedEvent'
 
 export const findAllUsers = async (): Promise<UserAggregator[]> => {
   await db()
-  const events = await Event.find({
-    name: { $in: [USER_REGISTRED_EVENT, USER_UPDATED_EVENT] }
-  }).exec()
+  const events = await Event.find().exec()
   return new UsersAggregator().applyEvents(events).users
 }
 
